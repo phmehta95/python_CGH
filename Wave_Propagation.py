2af86d74594e8be9c3e1e94ee51c13b4ec4d9596
Pruthvi Mehta <qthedesperate@gmail.com>
2024-02-21 12:58:31 +0000

    first commit

---
+++ Wave_Propagation.py
@@ -0,0 +1,11 @@
+#Simulates ability of function_propagate to simulate the propagation of complex wave fields
+#Original code by Nicholas Pegard @UNC, this is a re-write in python (for people who don't have access to a MATLAB license)
+
+#######Defining parameters of the simulation#############
+#Wavelength
+lambda = 1.0e-6
+#Size of pixel in metres
+ps = 10.0e-6
+#Number of data points along x-axis nd y-axis
+lx = 300
+ly = 300
