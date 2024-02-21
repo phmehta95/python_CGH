6f928be21aba0dca4db0eb4adc2b50cae35947c7
Pruthvi Mehta <qthedesperate@gmail.com>
2024-02-21 14:27:58 +0000

    created mesh grid

--- Wave_Propagation.py
+++ Wave_Propagation.py
@@ -1,11 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
 #Simulates ability of function_propagate to simulate the propagation of complex wave fields
 #Original code by Nicholas Pegard @UNC, this is a re-write in python (for people who don't have access to a MATLAB license)
 
 #######Defining parameters of the simulation#############
 #Wavelength
-lambda = 1.0e-6
+wav = 1.0e-6
+print(wav)
 #Size of pixel in metres
 ps = 10.0e-6
-#Number of data points along x-axis nd y-axis
+print(ps)
+#Number of data points along x-axis and y-axis
 lx = 300
 ly = 300
+#Propoagation distance of wave in meters in 1cm increments
+prop_dist = np.linspace(0,0.01,20)
+#print(prop_dist)
+
+#Creating co-ordinate grid for plotting wavefunction
+ux = np.linspace(0,1,300)
+ux = ux*ps
+ux = 1000*(ux-np.mean(ux))
+
+print(ux)
+
+uy = np.linspace(0,1,300)
+uy = uy*ps
+uy = 1000*(uy-np.mean(uy))
+
+print(uy)
