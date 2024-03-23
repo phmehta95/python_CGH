1425d7a5229789ff06d8a7018e46c18a957d250a
Pruthvi Mehta <qthedesperate@gmail.com>
2024-03-23 19:02:11 +0000

    debug

--- Wave_Propagation.py
+++ Wave_Propagation.py
@@ -7,7 +7,7 @@ import function_input
 import function_propagate
 from function_input import function_greylevel
 from function_propagate import function_propagate
-
+import cmath
 
 #Simulates ability of function_propagate to simulate the propagation of complex wave fields
 #Original code by Nicholas Pegard @UNC, this is a re-write in python (for people who don't have access to a MATLAB license)
@@ -42,7 +42,7 @@ uy = 1000*(uy-np.mean(uy))
 #print(uy)
 #Create 2D grid of above vectors
 xx,yy = np.meshgrid(ux,uy)
-#print(xx,yy)
+print(xx,yy)
 
 #Import a test image
 Image = function_greylevel("Image1.jpg")
@@ -54,14 +54,14 @@ Image = -Image
 
 
 #Creating complex field by setting amplitude and phase
-Amplitude = np.sqrt(Image)
+Amplitude = np.emath.sqrt(Image)
 #print (Amplitude)
 #Select one of the options below for phase of wavefunction
 Phase = np.zeros((lx,ly))
 #Phase = 2*np.pi*rand(lx,ly)
 
 ComplexField = Amplitude * np.exp(1j*Phase)
-
+print(ComplexField)
 #Display Amplitude and Phase at z = 0
 #Create a figure and define subplots
 fig,ax = plt.subplots(1,3,figsize=(15,5))
@@ -80,52 +80,52 @@ plt.ylabel('Y [mm]')
 plt.title('Intensity at z=0')
 
 # Plot 2: Amplitude at z=0
-plt.subplot(2, 3, 2)
-plt.imshow(abs(ComplexField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
-plt.axis('image')
-plt.xlabel('X [mm]')
-plt.ylabel('Y [mm]')
-plt.title('Amplitude at z=0')
+#plt.subplot(2, 3, 2)
+#plt.imshow(abs(ComplexField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
+#plt.axis('image')
+#plt.xlabel('X [mm]')
+#plt.ylabel('Y [mm]')
+#plt.title('Amplitude at z=0')
 
 #Plot 3: Phase at z=0
-plt.subplot(2, 3, 3)
-plt.imshow(np.angle(ComplexField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
-plt.axis('image')
-plt.xlabel('X [mm]')
-plt.ylabel('Y [mm]')
-plt.title('Phase at z=0')
+#plt.subplot(2, 3, 3)
+#plt.imshow(np.angle(ComplexField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
+#plt.axis('image')
+#plt.xlabel('X [mm]')
+#plt.ylabel('Y [mm]')
+#plt.title('Phase at z=0')
 
 
-fig, ax = plt.subplots(2, 3, figsize=(15, 10))
+#fig, ax = plt.subplots(2, 3, figsize=(15, 10))
 
-for j in range(len(prop_dist)):
+#for j in range(len(prop_dist)):
     # Propagate the field
-    NewField = function_propagate(ComplexField, wav, prop_dist[j], ps, ps)
+#    NewField = function_propagate(ComplexField, wav, prop_dist[j], ps, ps)
 
     # Display propagated field
-    ax[0, 1].cla()
-    ax[0, 1].imshow(np.abs(NewField)**2, extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
-    ax[0, 1].axis('image')
-    ax[0, 1].set_xlabel('X [mm]')
-    ax[0, 1].set_ylabel('Y [mm]')
-    ax[0, 1].set_title(f'Propagated Intensity z = {prop_dist[j]} m')
+#    ax[0, 1].cla()
+#    ax[0, 1].imshow(np.abs(NewField)**2, extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
+#    ax[0, 1].axis('image')
+#    ax[0, 1].set_xlabel('X [mm]')
+#    ax[0, 1].set_ylabel('Y [mm]')
+#    ax[0, 1].set_title(f'Propagated Intensity z = {prop_dist[j]} m')
 
-    ax[0, 2].cla()
-    ax[0, 2].imshow(np.abs(NewField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
-    ax[0, 2].axis('image')
-    ax[0, 2].set_xlabel('X [mm]')
-    ax[0, 2].set_ylabel('Y [mm]')
-    ax[0, 2].set_title(f'Amplitude at z = {prop_dist[j]} m')
+#    ax[0, 2].cla()
+#    ax[0, 2].imshow(np.abs(NewField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
+#    ax[0, 2].axis('image')
+#    ax[0, 2].set_xlabel('X [mm]')
+#    ax[0, 2].set_ylabel('Y [mm]')
+#    ax[0, 2].set_title(f'Amplitude at z = {prop_dist[j]} m')
 
-    ax[1, 0].cla()
-    ax[1, 0].imshow(np.angle(NewField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
-    ax[1, 0].axis('image')
-    ax[1, 0].set_xlabel('X [mm]')
-    ax[1, 0].set_ylabel('Y [mm]')
-    ax[1, 0].set_title(f'Phase at z = {prop_dist[j]} m')
+#    ax[1, 0].cla()
+#    ax[1, 0].imshow(np.angle(NewField), extent=(uy.min(), uy.max(), ux.min(), ux.max()), cmap='gray')
+#    ax[1, 0].axis('image')
+#    ax[1, 0].set_xlabel('X [mm]')
+#    ax[1, 0].set_ylabel('Y [mm]')
+#    ax[1, 0].set_title(f'Phase at z = {prop_dist[j]} m')
 
-    plt.draw()
-    plt.pause(0.1)  # Adjust the pause duration if needed
+#    plt.draw()
+#    plt.pause(0.1)  # Adjust the pause duration if needed
 
 # Show the plots
 plt.show()
--- function_input.py
+++ function_input.py
@@ -6,21 +6,29 @@ from PIL import Image
 import PIL
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
-
+import cv2
 
 #Defining the function which produces a grey-level image
 def function_greylevel(pathimage):
-    img = Image.open(pathimage)
-#    img = float(img)
-    img = np.mean(img, axis = 2)
+    img = cv2.imread(pathimage)
     
+    print (img)
+    img = img.astype(float)
+    #img = np.mean(img, axis = 2)
+    img = img/100.0
+    print (img)
     #Subtract background
     img = img - img.min()
+    print(img)
     img = img/img.max()
+    print(img)
     #Invert greyscale
     img = 1-img
+    print(img)
     img = img/img.sum()
+    print(img)
     img = np.matrix.transpose(img)
+    print(img)
     return img
 #    print (type(img))
 #    int8_img = img.astype(np.uint8)
